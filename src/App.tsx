aac3554a55aa43f29918bd65f784a756cf9025c0
arismoraitis <aris.moraiths@gmail.com>
2025-11-11 11:21:41 +0200

    feat: add useEffect example

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import Todo from "./components/Todo.tsx";
 
 function App() {
 
+
+//     useEffect(() => {
+// const id: number = setInterval(() => console.log("tick"), 1000);
+// return () => clearInterval(id);
+// } , []);
+
+
   return (
     <>
 <Todo/>
